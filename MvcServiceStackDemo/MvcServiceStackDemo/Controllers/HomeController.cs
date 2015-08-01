44463a36295ba158516563bd6de699b801b3edad
Zahurul Chowdhury <zahurul@hotmail.com>
2015-08-01 10:09:36 +1000

    Adding MvcServiceStackDemo VS2012 solution

--- MvcServiceStackDemo/MvcServiceStackDemo/Controllers/HomeController.cs
+++ MvcServiceStackDemo/MvcServiceStackDemo/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcServiceStackDemo.Models;
 
 namespace MvcServiceStackDemo.Controllers
 {
@@ -15,16 +16,28 @@ namespace MvcServiceStackDemo.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactModel contactModel)
+        {
+            if (SendMail(contactModel))
+            {
+                ViewBag.MailSent = true;
+            }
+            return View();
+        }
+
+        public bool SendMail(ContactModel mail)
+        {
+            return true;
         }
     }
 }
---
+++ MvcServiceStackDemo/MvcServiceStackDemo/Models/ContactModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcServiceStackDemo.Models
+{
+    public class ContactModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string WebSite { get; set; }
+        public string Comment { get; set; }
+    }
+}
